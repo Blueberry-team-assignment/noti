71e515a3213f478a09485b9d68d83e91ad4d6f77
dongchan <dc.shin@okestro.com>
2025-01-15 22:29:44 +0900

    add field in user model

--- lib/domain/model/user_model.dart
+++ lib/domain/model/user_model.dart
@@ -3,11 +3,13 @@ class UserModel {
   final String email;
   final String name;
   final List<String>? recentFlowHistoryIds;
+  final bool isAuthUser;
 
   UserModel({
     required this.email,
     required this.uid,
     required this.name,
     this.recentFlowHistoryIds,
+    required this.isAuthUser,
   });
 }
