678105070484848ea717aeccc1733efa99be28f5
dongchan <dc.shin@okestro.com>
2025-01-23 13:26:26 +0900

    fix logout

--- lib/features/my_page/my_page_provider.dart
+++ lib/features/my_page/my_page_provider.dart
@@ -1,22 +1,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:noti_flutter/data/auth/auth_repository.dart';
+import 'package:noti_flutter/data/local_storage/shared_preferences_provider.dart';
 import 'package:noti_flutter/features/log_in/presentation/providers/user_provider.dart';
 import 'package:noti_flutter/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final myPageNotifierProvider =
     StateNotifierProvider.family<MyPageNotifier, MyPageState, UserState>(
         (ref, userState) {
   final authRepository = ref.watch(authRepositoryProvider);
-  return MyPageNotifier(authRepository, userState);
+  final sharedPreferences = ref.watch(sharedPreferencesProvider);
+  return MyPageNotifier(authRepository, userState, sharedPreferences);
 });
 
 class MyPageNotifier extends StateNotifier<MyPageState> {
   final AuthRepository _authRepository;
   final UserState _userState;
+  final SharedPreferencesAsync _sharedPreferencesAsync;
 
   MyPageNotifier(
     this._authRepository,
     this._userState,
+    this._sharedPreferencesAsync,
   ) : super(MyPageState()) {
     state = MyPageState(user: _userState.user);
   }
@@ -24,6 +29,8 @@ class MyPageNotifier extends StateNotifier<MyPageState> {
   Future<void> logout() async {
     try {
       await _authRepository.logOut();
+
+      await _sharedPreferencesAsync.remove("uid");
 
       state = MyPageState(isLoading: false);
     } catch (e) {
